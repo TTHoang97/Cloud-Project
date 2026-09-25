13885864c9e77c5a3488714167c0978e62340bce
TTHoang97 <thomasthoang1997@gmail.com>
2026-09-25 10:53:38 -0400

    feat: add GitHub Actions OIDC provider and role

---
+++ Terraform/bootstrap/oidc.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_actions_trust" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:TTHoang97/Cloud-Project:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name                 = "cloud-project-github-actions"
+  assume_role_policy   = data.aws_iam_policy_document.github_actions_trust.json
+  max_session_duration = 3600
+}
+
+# v1: admin behind a strict trust policy. Scope down in v2.
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
